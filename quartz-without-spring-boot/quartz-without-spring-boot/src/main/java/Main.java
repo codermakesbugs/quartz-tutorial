8454d916031774771a1ccf0cce30226438ce8c8b
Linh Nguyen <linh.nt.git@gmail.com>
2023-10-07 20:46:59 +0700

    Skip trigger job by specific Calendar.

--- quartz-without-spring-boot/quartz-without-spring-boot/src/main/java/Main.java
+++ quartz-without-spring-boot/quartz-without-spring-boot/src/main/java/Main.java
@@ -1,10 +1,20 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+import org.quartz.impl.calendar.HolidayCalendar;
 
 public class Main {
   public static void main(String[] args) {
     try {
       Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+
+      HolidayCalendar cal = new HolidayCalendar();
+      cal.addExcludedDate(new SimpleDateFormat("dd.MM.yyyy").parse("08.10.2023"));
+
+      scheduler.addCalendar("test", cal, true, true);
+
+      scheduler.start();
 
       JobDetail jobDetailSimple =
           JobBuilder.newJob()
@@ -33,14 +43,16 @@ public class Main {
               .withIdentity("Hello-World-trigger-name CronTrigger")
               .forJob(jobDetailCron)
               .withSchedule(CronScheduleBuilder.cronSchedule("0/30 * * ? * * *"))
+              .modifiedByCalendar("test")
               .build();
 
-      scheduler.scheduleJob(jobDetailSimple, simpleTrigger);
+      //      scheduler.scheduleJob(jobDetailSimple, simpleTrigger);
       scheduler.scheduleJob(jobDetailCron, cronTrigger);
-      scheduler.start();
 
     } catch (SchedulerException e) {
       System.out.println(e.getMessage());
+    } catch (ParseException e) {
+      throw new RuntimeException(e);
     }
   }
 }
